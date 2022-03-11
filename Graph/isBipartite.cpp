8a8d758c3c51a3972a79c00eed9717684c88b0d5
Deep Fumtiya <deepfumtiya210@gmail.com>
2022-03-12 03:25:44 +0530

    Add Kahn's Algo to topological sort

--- Graph/isBipartite.cpp
+++ Graph/isBipartite.cpp
@@ -7,7 +7,7 @@ class Solution
 
     // A Bipartite Graph is a graph whose vertices can be divided into two independent sets, U and V such that every edge (u, v) either connects a vertex from U to V or a vertex from V to U. In other words, for every edge (u, v), either u belongs to U and v to V, or u belongs to V and v to U. We can also say that there is no edge that connects vertices of same set. This is gfg defination Bipartite graphs
 
-    // In simle words Bipartite graph 2 adjacent nodes have diff color/identity
+    // In simle words Bipartite graph is a graph that can be colored using two colors such that no 2 adjacent nodes have same color/identity
     // So Except Odd no. of cycle nodes containing graphs all other graph can be a bipartite graph 
 
 
@@ -34,7 +34,21 @@ public:
 	        }
 	    }
 	    return 1;
-	}
+	}// Time Complexity: O(N+E) && Auxillary Space: O(N+E)
+
+    // Function to detect odd/even no. of cycle in an undirected graph using DFS.
+    bool bipartiteDFS(int node, vector<int>adj[], int colorArr[]){
+	    if(colorArr[node]==-1)colorArr[node]=1;// we assign (no. 1)/(color any) to the node if it was not visided in array
+	    for(auto x:adj[node]){
+	        if(colorArr[x]==-1){// if color if not assigned we assign a color
+	            colorArr[x]=1-colorArr[node]; // We assign opposite color/no. of adjacent node to curr node
+	            if(!bipartiteDFS(x, adj, colorArr)) return 0;// if in any recursion we get false then it will be flase for all recursion
+	        }else if(colorArr[x]==colorArr[node]) return 0;// If 2 adjacent nodes have same color/no. i.e. its not a Bipartite graph
+	    }
+	    return 1;
+	}// Time Complexity: O(N+E) && Auxillary Space: O(N+E) + O(N) + O(N)
+
+
 
     // Function to detect cycle in an undirected graph.
     bool isBipartite(int V, vector<int> adj[])
@@ -46,12 +60,12 @@ public:
         // We do BFS/DFS to detect whether the graph has any odd numbered cycle
         for (int i = 0; i < V; i++){
             if (colorArr[i]==-1){
-                // if (cycleBFS(i, V, adj, vis)) return 1;
-                if (!bipartiteBFS(i, adj, colorArr)) return 0;
+                // if (!bipartiteBFS(i, adj, colorArr)) return 0;
+                if (!bipartiteDFS(i, adj, colorArr)) return 0;
             }
         }
         return 1;
-    } // Time Complexity: O(N+E) && Auxillary Space: O(N+E)
+    } 
 };
 
 int32_t main()
@@ -67,21 +81,21 @@ int32_t main()
     Solution Graph;
 
     // A graph with odd no. cycle node
-    // vector<int> adjList[5];
-    // adjList[1].push_back(0);
-    // adjList[0].push_back(2);
-    // adjList[2].push_back(1);
-    // adjList[0].push_back(3);
-    // adjList[3].push_back(4);
+    vector<int> adjList[5];
+    adjList[1].push_back(0);
+    adjList[0].push_back(2);
+    adjList[2].push_back(1);
+    adjList[0].push_back(3);
+    adjList[3].push_back(4);
 
-    // Graph.isBipartite(5, adjList)? cout << "Graph is Bipartite\n":cout << "Graph is not Bipartite\n";
+    Graph.isBipartite(5, adjList)? cout << "Graph is Bipartite\n":cout << "Graph is not Bipartite\n";
 
     // A graph with even no. cycle node
-    vector<int> adjList[3];
-    adjList[0].push_back(1);
-    adjList[1].push_back(2);
+    // vector<int> adjList[3];
+    // adjList[0].push_back(1);
+    // adjList[1].push_back(2);
  
-    Graph.isBipartite(3, adjList)?cout << "Graph is Bipartite\n": cout << "Graph isn't Bipartite\n";
+    // Graph.isBipartite(3, adjList)?cout << "Graph is Bipartite\n": cout << "Graph isn't Bipartite\n";
 
     return 0;
 }
---
+++ Graph/topologicalSort.cpp
@@ -0,0 +1,108 @@
+// Implement Topological Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    // Function to get Topological sort of given directed graph using DFS
+    void topologicalSortDFS(int node, vector<int> adj[], vector<bool> &vis, stack<int> &s)
+    {
+        vis[node] = 1;            // If a node is unvisited we mark it visted in array
+        for (auto it : adj[node]) // Recur for all the vertices adjacen to this vertex
+        {
+            if (!vis[it]) // If the node is not visited we do recursiveDFS search for that node
+                topologicalSortDFS(it, adj, vis, s);
+        }
+        s.push(node); // After coming out of recursion call we push that node into our stack
+    } // Time Complexity: O(N+E) &&  Space: O(N+E +N +N)=O(3N+E)
+
+    // Function to get Topological sort of given directed graph using Kahn's Algo
+    vector<int> topologicalSortBFS(int V, vector<int> adj[], vector<int>&indegree){
+	    vector<int>ans;
+	    queue<int>q; // We maintain a queue 
+	    // And push each node that has 0 indegree edges
+	    for(int i=0; i<V; ++i)if(!indegree[i])q.push(i);
+	    while(!q.empty()){ //We run BfS till the queue is empty
+	        int node=q.front();
+	        for(auto it: adj[node]){
+	            indegree[it]--; //Once we visted that node we reduce it's indegree by 1
+	            if(!indegree[it])q.push(it); //If a nodes indegree becomes 0 we push that into our queue
+	        }
+	        ans.push_back(node); //Once we visted all adjacent nodes of our queue's front element we push it to our ans
+	        q.pop(); // And pop that node out of queue
+	    }
+	    return ans;
+	}// Time Complexity: O(N+E) &&  Space: O(N+N)=O(2N)
+
+public:
+    // Function to return list containing vertices in Topological order.
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        // Code here
+        // Method 1: Using DFS algo
+        // vector<bool> vis(V, 0);
+        // stack<int> s; // We maintain a stack and push each node into it after each recursion call is over
+        // for (int i = 0; i < V; ++i) // If the given graph is in components we use this for loop to move to next unconnected component
+        // {               
+        //     if (!vis[i]) // If the node is not visited we do DFS search for that node
+        //         topologicalSortDFS(i, adj, vis, s);
+        // }
+
+        // vector<int> ans;
+        // // We push all nodes in stack into our array till its empty
+        // while (!s.empty())
+        // {
+        //     ans.push_back(s.top());
+        //     s.pop();
+        // }
+        // return ans;
+
+        // Method 2: Using Kahn's Algorithm i.e also a BFS Algo
+        //Instead of visted array we make a indegree Array
+        vector<int>indegree(V, 0);// indegree of a node is no. of edges connecting towards that particular node
+	    for(int i=0; i<V; ++i){//We traverse through each node
+	        for(auto it:adj[i])indegree[it]++; //If that node has adjacent nodes we do thats that adjacent node's +1 in array
+	    }
+	    return topologicalSortBFS(V, adj, indegree);
+    }
+};
+
+int32_t main()
+{
+    /* Constructed Graph from given input as
+         0
+       / | \
+      /  |  \
+     1   2   3
+        /
+       4
+
+    */
+
+    Solution Graph;
+     
+    // Graph 1
+    vector<int> adjList[5];
+    adjList[0].push_back(1);
+    adjList[0].push_back(2);
+    adjList[0].push_back(3);
+    adjList[2].push_back(4);
+
+    // Graph 2
+    // vector<int> adjList[6];
+    // adjList[5].push_back(2);
+    // adjList[5].push_back(0);
+    // adjList[4].push_back(0);
+    // adjList[4].push_back(1);
+    // adjList[2].push_back(3);
+    // adjList[3].push_back(1);
+
+    cout << "Following is a Topological Sort of the given Directed Acycalic Graph : ";
+    vector<int> ans = Graph.topoSort(5, adjList);
+    for (auto x : ans)
+        cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
